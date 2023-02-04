101f0cb3a235c5a2f46d608b4d4f970cd314b271
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-04 13:13:52 +0100

    fiksiranje vel i vis

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -11,7 +11,7 @@ public class AppFX extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Hello, World!");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
