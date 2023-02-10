802ef10f7df7e8a553c4c71bd74f114f72933849
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-10 18:27:46 +0100

    dodana ikonica

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -12,7 +13,8 @@ public class AppFX extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("Hello, World!");
+        primaryStage.setTitle("Aplikacija");
+        primaryStage.getIcons().add(new Image("https://icons-for-free.com/iconfiles/png/512/bookshelf+library+icon-1320087270870761354.png"));
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
