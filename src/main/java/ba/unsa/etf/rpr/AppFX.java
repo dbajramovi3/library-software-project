65ac7499b01b093687e05c7f17b5773fe1270aa6
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-13 11:32:10 +0100

    Otvaranje novog prozora

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -7,17 +7,20 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 
 public class AppFX extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Aplikacija");
         primaryStage.getIcons().add(new Image("https://icons-for-free.com/iconfiles/png/512/bookshelf+library+icon-1320087270870761354.png"));
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+      //  Image img = new Image("https://i.pinimg.com/originals/8c/74/bb/8c74bb4e959d7211e27c10b5980cb970.jpg");
         primaryStage.setResizable(false);
         primaryStage.show();
     }
--- src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
@@ -1,8 +1,36 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class SampleController {
-    public void addBookActon(ActionEvent actionEvent) {
+
+    public Button addBookButtonid;
+
+    public void addBookActon(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/addBook.fxml"));
+        stage.setTitle("Aplikacija");
+        stage.getIcons().add(new Image("https://icons-for-free.com/iconfiles/png/512/bookshelf+library+icon-1320087270870761354.png"));
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
     }
+     public SampleController(){
+     }
+
+     @FXML
+    public void initialize(){
+
+     }
 }
