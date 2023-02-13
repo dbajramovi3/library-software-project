fa29a936936191b6fd8e002f076fbb9774e4ef27
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-13 11:54:04 +0100

    Modifikovan addBookAction + komentari

--- src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
@@ -17,12 +17,26 @@ public class SampleController {
 
     public Button addBookButtonid;
 
+    /* Napravili smo akciju za addBookAction s tim da smo je povezali tako da ne moramo povezivat putme addBook.fxml u tekst kodu
+    Ovo je prva akcija koju smo napravili i nije nam trebao listener jer nismo unosili podatke iz razloga sto app ne zahtjeva log in formu
+    listener uvijek pozivas nad propertijem, tacnije textProperty.getListener() jer su stringovi u pitanju
+    znaci uvezali smo 2 prozora ali ne kroz fxml kod nego kroz java kod
+
+   OVAJ kod u addBookAction smo mogli napisati kao kod u AppFX jer ne saljes nikakve parametre
+   Ali napisao sam ga cisto iz razloga jer sam pratio tutorijal 8, znaci kad saljemo parametre pisemo ovaj kod ispod s tim da:
+      addBookController controller = new addBookController(); napises kao
+         addBookController controller = new addBookController(imeId.getText(), prezimeId.getText()); jer saljemo 2 parametra
+         String ime i prezime
+     */
     public void addBookActon(ActionEvent actionEvent) throws IOException {
         Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/addBook.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/addBook.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("/fxml/addBook.fxml"));
+        addBookController controller = new addBookController();
+        loader.setController(controller);
         stage.setTitle("Aplikacija");
         stage.getIcons().add(new Image("https://icons-for-free.com/iconfiles/png/512/bookshelf+library+icon-1320087270870761354.png"));
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setScene(new Scene(loader.<Parent>load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.setResizable(false);
         stage.show();
     }
