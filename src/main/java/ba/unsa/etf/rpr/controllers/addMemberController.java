95089320bb1ff2b3c6638bd26d371506f32176a2
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-14 16:36:55 +0100

    ispravka na labelama i gui

--- src/main/java/ba/unsa/etf/rpr/controllers/addMemberController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/addMemberController.java
@@ -1,4 +1,38 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class addMemberController {
+    public Button saveButtonId;
+    public Button cancelButtonId;
+    public TextField nameId;
+    public TextField lastNameId;
+    public TextField emailId;
+    public TextField memberId;
+    public Label NeispravanTextId;
+
+    public void saveAction(ActionEvent actionEvent) {
+    }
+
+    public void cancelAction(ActionEvent actionEvent) {
+    }
+
+    public addMemberController() {
+
+    }
+
+    @FXML
+    public void Initialize(){
+        memberId.textProperty().addListener((obs, newValue, oldValue) -> {
+            if(newValue.length()==4){
+                NeispravanTextId.setText("");
+            }
+            else NeispravanTextId.setText("Id mora imati 5 karaktera!");
+        });
+
+    }
 }
