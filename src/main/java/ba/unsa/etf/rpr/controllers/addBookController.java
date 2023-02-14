835a2a7d9a61f687ba86071f2be6d60563d4cd6b
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-14 15:55:36 +0100

    modifikacije u kodu kod kontrolera addBook

--- src/main/java/ba/unsa/etf/rpr/controllers/addBookController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/addBookController.java
@@ -1,5 +1,26 @@
 package ba.unsa.etf.rpr.controllers;
 
-public class addBookController {
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
+public class addBookController {
+    public Button saveButtonId;
+    public Button cancelButtonId;
+    public TextField bookTitleId;
+    public TextField bookId;
+    public TextField authorId;
+
+    public void saveAction(ActionEvent actionEvent) {
+    }
+
+    public void cancelAction(ActionEvent actionEvent) {
+    }
+
+    public addBookController() {
+    }
+
+    public void initialize(){
+
+    }
 }
