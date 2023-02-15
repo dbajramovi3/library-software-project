751186cee49b006bdfe73fe3592348747d06092c
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-15 01:53:43 +0100

    zavrsavanje registerBook.fxml zbog modifikacije

--- src/main/java/ba/unsa/etf/rpr/controllers/registerBookController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/registerBookController.java
@@ -4,23 +4,27 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class registerBookController {
     public Label bookId;
     public Label memberId;
-    public Button backId;
+    public Button closeId;
     private String knjiga, memberid;
     public registerBookController(String knjiga, String memberid){
         this.knjiga = knjiga;
         this.memberid = memberid;
     }
 
+    public void closeAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) bookId.getScene().getWindow();
+        stage.close();
+
+    }
+
     @FXML
     public void initialize(){
         bookId.setText(knjiga);
         memberId.setText(memberid);
-    }
-
-    public void backAction(ActionEvent actionEvent) {
     }
 }
