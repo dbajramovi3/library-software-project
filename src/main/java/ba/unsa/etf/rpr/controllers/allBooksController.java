cd7a730a307cb7e5116efbc301eae319b533a81e
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-15 02:20:22 +0100

    Close button funkcionalnosti

--- src/main/java/ba/unsa/etf/rpr/controllers/allBooksController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/allBooksController.java
@@ -1,4 +1,11 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class allBooksController {
+    public Button closeId;
+
+    public void closeAction(ActionEvent actionEvent) {
+    }
 }
