9ad48d10a7ace77b8826e249af8bfba1a34f0212
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-15 19:48:12 +0100

    controlleri dodavanje id-ova/akcije

--- src/main/java/ba/unsa/etf/rpr/controllers/searchBookController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/searchBookController.java
@@ -1,6 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class searchBookController {
 
 
+    public TextField enterBookNameId;
+    public Label labelId;
+    public Button closeButtonId;
+
+    public void closeAction(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/controllers/searchMemberController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/searchMemberController.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class searchMemberController {
+    public TextField enterMemberNameId;
+    public Label labelId;
+    public Button closeButtonId;
+
+    public void closeAction(ActionEvent actionEvent) {
+    }
 }
