e332267fb6246775499f8a8caffc495c0fa5f948
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-15 19:04:57 +0100

    allMembers gui

--- src/main/java/ba/unsa/etf/rpr/controllers/allMembersController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/allMembersController.java
@@ -1,4 +1,16 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
 public class allMembersController {
+    public Button closeId;
+    public ListView listView;
+
+    public void closeAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) closeId.getScene().getWindow();
+        stage.close();
+    }
 }
