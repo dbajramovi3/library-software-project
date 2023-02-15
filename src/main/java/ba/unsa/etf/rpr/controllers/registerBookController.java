1d383d1bb8afdc1c03c9f961e17346dac0db86b1
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-15 01:51:05 +0100

    dodavanje akcije na registerdbook

--- src/main/java/ba/unsa/etf/rpr/controllers/registerBookController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/registerBookController.java
@@ -1,11 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class registerBookController {
     public Label bookId;
     public Label memberId;
+    public Button backId;
     private String knjiga, memberid;
     public registerBookController(String knjiga, String memberid){
         this.knjiga = knjiga;
@@ -16,5 +19,8 @@ public class registerBookController {
     public void initialize(){
         bookId.setText(knjiga);
         memberId.setText(memberid);
+    }
+
+    public void backAction(ActionEvent actionEvent) {
     }
 }
