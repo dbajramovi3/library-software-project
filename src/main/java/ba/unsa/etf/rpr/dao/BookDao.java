f93c0a09e7564554d48cd43c1f6145b4668c6004
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-16 18:31:26 +0100

    Added getByName method in BookDao

--- src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
@@ -3,5 +3,8 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Book;
 import ba.unsa.etf.rpr.exception.LibraryException;
 
+import java.util.List;
+
 public interface BookDao extends Dao<Book>{
+    public List<Book> getByName(String name) throws LibraryException;
 }
