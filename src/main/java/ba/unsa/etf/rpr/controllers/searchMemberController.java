89291012605a97a6dd7e1f11af53ca004e995642
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-17 02:52:15 +0100

    Search Member funkcionise

--- src/main/java/ba/unsa/etf/rpr/controllers/searchMemberController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/searchMemberController.java
@@ -2,19 +2,38 @@ package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.BookManager;
 import ba.unsa.etf.rpr.business.MemberManager;
+import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.domain.Member;
+import ba.unsa.etf.rpr.exception.LibraryException;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.util.List;
+
 public class searchMemberController {
     public TextField enterMemberNameId;
     public Label labelId;
     public Button closeButtonId;
+    public Button searchBttn;
+    public TextField enterMNameId;
     private MemberManager memberManager = new MemberManager();
     public void closeAction(ActionEvent actionEvent) {
         Stage stage = (Stage) labelId.getScene().getWindow();
         stage.close();
+    }
+
+    public void searchBttnAction(ActionEvent actionEvent) {
+        try {
+            List<Member> memberList = memberManager.getByName(enterMNameId.getText());
+            if(memberList.size()!=0) labelId.setText("Member is in the database");
+            else labelId.setText("Member is not in the database");
+        } catch (LibraryException e) {
+            System.out.println("EXCEPTION");
+            throw new RuntimeException(e);
+        }
+
     }
 }
