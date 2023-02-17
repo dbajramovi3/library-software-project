2ca677b7c01804cd9d9c68ecca5dc3173da04e92
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-17 19:37:22 +0100

    dodan buyController i prebaceni podaci preko fx:controller="ba.unsa.etf.rpr.controllers.buyController"

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/buyController.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class buyController {
+    public Button deleteId;
+
+    public void deleteAction(ActionEvent actionEvent) {
+    }
+}
