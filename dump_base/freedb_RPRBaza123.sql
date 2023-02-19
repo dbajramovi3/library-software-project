f2bb2c6359290f4c304dcb10ffa7d1017eeaf89a
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-19 23:45:08 +0100

    update dodana nova baza

--- dump_base/freedb_RPRBaza123.sql
+++ dump_base/freedb_RPRBaza123.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `freedb_RPRBaza123` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `freedb_RPRBaza123`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: sql.freedb.tech    Database: freedb_RPRBaza123
@@ -14,6 +16,114 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `author` varchar(45) NOT NULL,
+  `current_book_hold` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (19,'Harry Potter','JK Rowling',8),(20,'Jezeva Kucica','Irnes',10),(21,'Hobbit','adi',2);
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `member_cards`
+--
+
+DROP TABLE IF EXISTS `member_cards`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member_cards` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `member_id` int NOT NULL,
+  `activation_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `MEMBERID_idx` (`member_id`),
+  CONSTRAINT `MEMBERID` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member_cards`
+--
+
+LOCK TABLES `member_cards` WRITE;
+/*!40000 ALTER TABLE `member_cards` DISABLE KEYS */;
+INSERT INTO `member_cards` VALUES (9,17,'2023-02-19'),(10,18,'2023-02-19'),(11,19,'2023-02-19'),(12,20,'2023-02-19');
+/*!40000 ALTER TABLE `member_cards` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `members`
+--
+
+DROP TABLE IF EXISTS `members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `members` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `members`
+--
+
+LOCK TABLES `members` WRITE;
+/*!40000 ALTER TABLE `members` DISABLE KEYS */;
+INSERT INTO `members` VALUES (17,'Dino ','Bajramovic','dinobajramovic01@gmail.com'),(18,'Nikola','Tesla','nikolatesla@gmail.com'),(19,'Jusuf','Nurkic','juka@gmail.com'),(20,'ahmed','ahmedovic','ahmed@hotmail.com');
+/*!40000 ALTER TABLE `members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shopping_cart`
+--
+
+DROP TABLE IF EXISTS `shopping_cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shopping_cart` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `member_card_id` int NOT NULL,
+  `book_id` int NOT NULL,
+  `buy_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `memberCardId_idx` (`member_card_id`),
+  KEY `bookId_idx` (`book_id`),
+  CONSTRAINT `bookId` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  CONSTRAINT `memberCardId` FOREIGN KEY (`member_card_id`) REFERENCES `member_cards` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shopping_cart`
+--
+
+LOCK TABLES `shopping_cart` WRITE;
+/*!40000 ALTER TABLE `shopping_cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shopping_cart` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +134,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-19 23:35:07
+-- Dump completed on 2023-02-19 23:43:24
