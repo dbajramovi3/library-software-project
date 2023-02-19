4e7ea73693a415eae69276fb2205bed0397953f5
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-19 20:46:45 +0100

    Implemented initialize method in shoppingCartController.java

--- src/main/java/ba/unsa/etf/rpr/controllers/shoppingCartController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/shoppingCartController.java
@@ -1,20 +1,29 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.ShoppingCartManager;
+import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.domain.MemberCard;
 import ba.unsa.etf.rpr.domain.ShoppingCart;
 import ba.unsa.etf.rpr.exception.LibraryException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class shoppingCartController {
     public Button deleteBttnId;
-    public TableView tableView;
-ShoppingCartManager shoppingCartManager = new ShoppingCartManager();
+    public TableView<ShoppingCart> tableView;
+    public TableColumn<Integer, ShoppingCart> idColumn;
+    public TableColumn<LocalDate, ShoppingCart> buyDateColumn;
+    public TableColumn<MemberCard, ShoppingCart> memberCardIdColumn;
+    public TableColumn<Book, ShoppingCart> bookIdColumn;
+    ShoppingCartManager shoppingCartManager = new ShoppingCartManager();
     public void deleteAction(ActionEvent actionEvent) throws LibraryException {
 
         ShoppingCart selectedItem = (ShoppingCart) tableView.getSelectionModel().getSelectedItem();
@@ -30,6 +39,10 @@ ShoppingCartManager shoppingCartManager = new ShoppingCartManager();
     }
 
     public void initialize() throws LibraryException {
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        memberCardIdColumn.setCellValueFactory(new PropertyValueFactory<>("member_card_id"));
+        bookIdColumn.setCellValueFactory(new PropertyValueFactory<>("book_id"));
+        buyDateColumn.setCellValueFactory(new PropertyValueFactory<>("buy_date"));
         List<ShoppingCart> shoppingCartList = shoppingCartManager.getAll();
         ObservableList<ShoppingCart> observableList = FXCollections.observableList(shoppingCartList);
         tableView.setItems(observableList);
