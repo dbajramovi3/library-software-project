7e8e26b9cc87d3e2c91aa13a5e3ec4c6340bc111
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-19 22:47:29 +0100

    Welcoming to App.java terminal

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -6,6 +6,18 @@ package ba.unsa.etf.rpr;
  */
 public class App  {
     public static void main(String[] args) {
+        System.out.println("Hello, welcome to Library-Management System");
+        System.out.println("Please type one of the following options:");
+        System.out.println();
+        System.out.println("Type 1 to ADD a Book");
+        System.out.println("Type 2 to ADD a Member");
+        System.out.println("Type 3 to Search for a Book");
+        System.out.println("Type 4 to Search for a Member");
+        System.out.println("Type 5 to see the list of all Books");
+        System.out.println("Type 6 to see the list of all Members");
+        System.out.println("Type 7 to REGISTER a Book");
+        System.out.println("Type 8 to view the Shopping cart");
+
 
     }
 
