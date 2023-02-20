00ed292cb5984af7b04cdc98381d4854fde8b0f7
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-20 18:49:31 +0100

    BookDao && MemberDao javadoc spec

--- src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
@@ -6,9 +6,16 @@ import ba.unsa.etf.rpr.exception.LibraryException;
 import java.util.List;
 
 public interface BookDao extends Dao<Book>{
+
+    /**
+     * abstract method defined in BookDaoImpl
+     * @author dbajramovi3
+     * @param name
+     * @return
+     * @throws LibraryException
+     */
     public List<Book> getByName(String name) throws LibraryException;
 
-    public Book getByName2(String name) throws LibraryException;
 
 
 }
--- src/main/java/ba/unsa/etf/rpr/dao/MemberDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/MemberDao.java
@@ -6,7 +6,13 @@ import ba.unsa.etf.rpr.exception.LibraryException;
 import java.util.List;
 
 public interface MemberDao extends Dao<Member> {
-
+    /**
+     * 2 abstract methods defined in MemberDaoImpl
+     * @author dbajramovi3
+     * @param name
+     * @return
+     * @throws LibraryException
+     */
     public List<Member> getByName(String name) throws LibraryException;
 
     public List<Member> getByEmail(String email) throws LibraryException;
