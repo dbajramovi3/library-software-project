ab35906b17f425eced51fcb5a019526162507685
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-20 12:37:25 +0100

    idable javadoc documentation

--- src/main/java/ba/unsa/etf/rpr/domain/Idable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -4,6 +4,7 @@ import ba.unsa.etf.rpr.exception.LibraryException;
 
 /**
  * Interface that forces all POJO beans to have ID field. Name is stupid but per standard
+ * Every class form domain has an atribute named id, insted of BookId, MemberId...
  */
 public interface Idable{
 
