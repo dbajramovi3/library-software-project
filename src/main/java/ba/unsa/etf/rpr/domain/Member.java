e6b4651968795ac7c64760504dbb3332ad7d42bd
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-20 23:38:36 +0100

    third javadoc finishing touches

--- src/main/java/ba/unsa/etf/rpr/domain/Member.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Member.java
@@ -17,51 +17,114 @@ public class Member implements Idable, Serializable {
     private String last_name;
     private String email;
 
+    /**
+     * Member constructor
+     * @param id id
+     * @param name name
+     * @param last_name lastname
+     * @param email email
+     */
     public Member(int id, String name, String last_name, String email) {
         this.id = id;
         this.name = name;
         this.last_name = last_name;
         this.email = email;
     }
-public Member(){ }
+
+    /**
+     * Empty constructor
+     * @author dbajramovi3
+     */
+    public Member(){ }
+
+    /**
+     * getId method
+     * @return id
+     * @author dbajramovi3
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * setId method
+     * @param id id
+     * @author dbajramovi3
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * getName method
+     * @return name
+     * @author dbajramovi3
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * setName method
+     * @param name name
+     * @author dbajramovi3
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * getLast_name method
+     * @return last name
+     * @author dbajramovi3
+     */
     public String getLast_name() {
         return last_name;
     }
 
+    /**
+     * setLast_name method
+     * @param last_name ln
+     * @author dbajramovi3
+     */
     public void setLast_name(String last_name) {
         this.last_name = last_name;
     }
 
+    /**
+     * getEmail method
+     * @return email
+     * @author dbajramovi3
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * setEmail method
+     * @param email em
+     * @author dbajramovi3
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * toString method
+     * @return ispis
+     * @author dbajramovi3
+     */
     @Override
     public String toString() {
         return "  " + id + "   "
                 + name + "  " + last_name + "   " + email;
     }
 
+    /**
+     * equals method
+     * @param o o
+     * @return comparison
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -70,6 +133,11 @@ public Member(){ }
         return getId() == members.getId() && Objects.equals(getName(), members.getName()) && Objects.equals(getLast_name(), members.getLast_name()) && Objects.equals(getEmail(), members.getEmail());
     }
 
+    /**
+     * hashCode method
+     * @return int
+     * @author dbajramovi3
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getId(), getName(), getLast_name(), getEmail());
