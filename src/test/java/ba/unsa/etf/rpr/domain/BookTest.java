4567edb4393782f75113f11c1d7f07ba0f374907
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-02-21 02:10:07 +0100

    javadoc na unit testovima

--- src/test/java/ba/unsa/etf/rpr/domain/BookTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/BookTest.java
@@ -16,11 +16,18 @@ Book book = new Book(1, "Harry Potter", "JK Rowling", 10);
         assertEquals("Harry Potter", book.getTitle());
     }
 
+    /**
+     * getIdTest
+     * @author dbajramovi3
+     */
     @Test
     void getId() {
         assertEquals(1, book.getId());
     }
-
+    /**
+     * setIdTest
+     * @author dbajramovi3
+     */
     @Test
     void setId() {
         Book book = new Book();
@@ -29,58 +36,78 @@ Book book = new Book(1, "Harry Potter", "JK Rowling", 10);
         book.setId(456);
         assertEquals(456, book.getId());
     }
-
+    /**
+     * getTitleTest
+     * @author dbajramovi3
+     */
     @Test
     void testGetTitle() {
         Book knjiga = new Book(1,"Hobbit", "amar", 12);
         assertEquals("Hobbit", knjiga.getTitle());
 
     }
-
+    /**
+     * setTitletest
+     * @author dbajramovi3
+     */
     @Test
     void setTitle() {
         Book book = new Book();
         book.setTitle("Avatar");
         assertEquals("Avatar", book.getTitle());
     }
-
+    /**
+     * getAuthorTest
+     * @author dbajramovi3
+     */
     @Test
     void getAuthor() {
         Book knjiga = new Book(1,"Hobbit", "amar", 12);
         assertEquals("amar", knjiga.getAuthor());
     }
-
+    /**
+     * setAuthorTest
+     * @author dbajramovi3
+     */
     @Test
     void setAuthor() {
         Book book = new Book();
         book.setAuthor("JK Rowling");
         assertEquals("JK Rowling", book.getAuthor());
     }
-
+    /**
+     * getCurrentBookHoldTest
+     * @author dbajramovi3
+     */
     @Test
     void getCurrent_book_hold() {
         Book book = new Book();
         book.setCurrent_book_hold(12);
         assertEquals(12,book.getCurrent_book_hold());
     }
-
+    /**
+     * setCurrentBookHoldTest
+     * @author dbajramovi3
+     */
     @Test
     void setCurrent_book_hold() {
         Book book = new Book(1,"adad","adad", 13);
         assertEquals(13, book.getCurrent_book_hold());
     }
-
+    /**
+     * toStringTest
+     * @author dbajramovi3
+     */
     @Test
     void testToString() {
         Book book = new Book(1, "Hobbit", "Dinotelo", 0);
-
-        // Act
         String result = book.toString();
-
-        // Assert
         assertEquals("  1   Hobbit   0", result);
     }
-
+    /**
+     * EqualsTest
+     * @author dbajramovi3
+     */
     @Test
     void testEquals() {
         Book book1 = new Book(1, "Hobbit", "Dinotelo", 0);
@@ -94,7 +121,10 @@ Book book = new Book(1, "Harry Potter", "JK Rowling", 10);
         assertFalse(book2.equals(null));
         assertFalse(book2.equals("not a book"));
     }
-
+    /**
+     * hashCodeTest
+     * @author dbajramovi3
+     */
     @Test
     void testHashCode() {
         Book book1 = new Book(1, "Title 1", "Author 1", 0);
